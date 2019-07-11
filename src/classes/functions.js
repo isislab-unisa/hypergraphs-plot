e6712f9713ef226b48dedc9ab3945be5937b68a7
Marti <vittoriobaldi18@gmail.com>
2019-07-11 16:30:55 +0200

    Add functions

--- src/classes/functions.js
+++ src/classes/functions.js
@@ -1,40 +1,101 @@
-/*funzionalità comuni
- * G.getNodes()
- * G.getLinks()
- * G.getNumNodes()
- * G.getNumLinks()
- * G.addNode()
- * G.addLinks()
+import JSNetworkXError from '../exceptions/HypergraphsPlotError';
+
+
+/**
+ * Return a copy of the hypergraph nodes in a list.
+ *
+ * @param {Hypergraph} HG Hypergraph
+ * @return {Array} List of nodes
  */
-
 export function getNodes(HG){
-
+    return HG.getNodes();
 }
 
+/**
+ * Return a copy of the hypergraph links in a list.
+ *
+ * @param {Hypergraph} HG Hypergraph
+ * @return {Array} List of links
+ */
 export function getLinks(HG){
-
+    return HG.getLinks();
 }
 
+/**
+ * Return a copy of the hypergraph node-link with value in a list.
+ * 
+ * @param {Hypergraph} HG Hypergraph
+ * @return {Array of Objects} Array of objects (node,link,value)
+ */
+export function getNodesLinks(HG){
+    return HG.getNodesLinks();
+}
+
+/**
+ * Return the number of nodes in the hypergraph.
+ *
+ * @param {Hypergraph} HG Hypergraph
+ * @return {number} Number of nodes
+ */
 export function getNumNodes(HG){
-
+    return HG.getNumNodes();
 }
 
+/**
+ * Return the number of edges in the hypergraph.
+ *
+ * @param {Hypergraph} HG Hypergraph
+ * @return {Int} Number of links
+ */
 export function getNumLinks(HG){
-
+    return HG.getNumLinks();
 }
 
-export function addNode(HG){
-
+/**
+ * Get node attributes from Hypergraph
+ * 
+ * @param {Hypergraph} HG Hyperpgrah
+ * @param {String} idNode id of the node
+ * @return {!Map} Dictionary of attributes keyed by node 
+ */
+export function getNodeAttributes(HG,idNode){
+    var dict = new Map();
+    HG.nodes.forEach(function(node,i){
+        if(node.id === idNode){
+            dict.set(node.id, node.attributes);
+        }
+    })
+    return dict;
 }
 
-export function addLinks(HG){
-
+/**
+ * Set node attributes from Hypergraph nodes
+ * 
+ * @param {Hypergraph} HG Hyperpgrah
+ * @param {String} idNode id of the node
+ * @param {String} attributes attributes
+ */
+export function setNodeAttributes(HG,idNode,attributes){
+    HG.nodes.forEach(function(node,i){
+        if(node.id === idNode){
+            node.attributes = attributes;
+        }
+    })
 }
 
-export function removeNode(HG){
-
-}
-
-export function removeLinks(HG){
-
+/**
+ * Get links attributes from Hypergraph
+ * 
+ * @param {Hypergraph} HG Hyperpgrah
+ * @param {String} idLink id of the link
+ * @return {!Map} Dictionary of attributes keyed by node 
+ */
+export function getLinkAttributes(HG,idLink){
+    var dict = new Map();
+    HG.links.forEach(function(link,i){
+        if(link.id === idLink){
+            dict.set(link.id, link.nodes);
+        }
+    })
+    return dict;
 }
