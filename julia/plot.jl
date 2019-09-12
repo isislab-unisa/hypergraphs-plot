85e2d3c927c608228db1b5d48ddd9a99b47763f5
VittorioBaldi <vittoriobaldi18@gmail.com>
2019-09-12 18:00:08 +0200

    Add Keyword for the ColorEdge

--- julia/plot.jl
+++ julia/plot.jl
@@ -10,25 +10,30 @@ params: h: Hyperhgraph
 example: plot(h, type="color-edge")
          plot("data.json", type="venn")
 =#
-function plot(JSONString::String; type="color-edge")
+function plot(JSONString::String;type="color-edge",colorNodes="#D3D3D3",colorEdges="default",sizeNodes=10)
     plotting(JSON.json(JSON.parsefile(JSONString)),type)
 end
 
-function plot(h::Hypergraph; type="color-edge")
+function plot(h::Hypergraph;type="color-edge",colorNodes="#D3D3D3",colorEdges="default",sizeNodes=10)
+    Preferences=Dict()
+    Preferences["colorNodes"]=colorNodes;
+    Preferences["colorEdges"]=colorEdges;
+    Preferences["sizeNodes"]=sizeNodes;
+
     generateFileJSON(h) #generate a json file into default path
 
     f = open("data.json","r")
     data = read(f,String)
     close(f)
-    plotting(data,type)
+    plotting(data,Preferences,type)
 
 end
 
-function plotting(data, type)
+function plotting(data,Preferences,type)
     if type=="venn"
         plotVenn(data)
     elseif type=="color-edge"
-        plotColorEdge(data)
+        plotColorEdge(data,Preferences)
     elseif type=="radal"
         plotRadal(data)
     end
